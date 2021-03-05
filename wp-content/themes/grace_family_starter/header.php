1bc694075c1d3b22d5776351ec283511e3fb2748
James Hamann <hamann.james87@gmail.com>
2021-03-05 13:24:09 -0800

    add meta viewport tag

--- wp-content/themes/grace_family_starter/header.php
+++ wp-content/themes/grace_family_starter/header.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
   <title>Grace Family Church of Everett</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="<?php echo(get_template_directory_uri() . '/assets/css/style.css'); ?>">
